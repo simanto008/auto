a2846acecc523c4ea135e402b24e226927b62527
ashrafulmulla99 <ashrafulmulla99@gmail.com>
2025-09-30 16:20:42 +0600

    libs done

---
+++ libs/logger.py
@@ -0,0 +1,101 @@
+
+
+import asyncio
+import logging
+from traceback import format_exc
+
+from telethon import TelegramClient
+from telethon.errors.rpcerrorlist import FloodWaitError
+
+from functions.config import Var
+
+logging.basicConfig(
+    format="%(asctime)s || %(name)s [%(levelname)s] : %(message)s",
+    handlers=[
+        logging.FileHandler("AutoAnimeBot.log", mode="w", encoding="utf-8"),
+        logging.StreamHandler(),
+    ],
+    level=logging.INFO,
+    datefmt="%m/%d/%Y, %H:%M:%S",
+)
+LOGS = logging.getLogger("AutoAnimeBot")
+TelethonLogger = logging.getLogger("Telethon")
+TelethonLogger.setLevel(logging.INFO)
+
+LOGS.info(
+    f"""
+                            Auto Anime Bot
+                ©️ t.me/kAiF_00z (github.com/kaif-00z)
+                        {Var.__version__} (original)
+                             (2023-25)
+                        [All Rigths Reserved]
+
+    """
+)
+
+
+class Reporter:
+    def __init__(self, client: TelegramClient, file_name: str):
+        self.client: TelegramClient = client
+        self.file_name = file_name
+        self.msg = None
+
+    async def alert_new_file_founded(self):
+        await self.awake()
+        msg = await self.client.send_message(
+            Var.MAIN_CHANNEL if Var.LOG_ON_MAIN else Var.LOG_CHANNEL,
+            f"**New Anime Released**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Downloading...`",
+        )
+        self.msg = msg
+
+    async def started_compressing(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Downloaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Encoding...`",
+        )
+        return self.msg
+
+    async def started_renaming(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Downloaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Renaming...`",
+        )
+
+    async def started_uploading(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Encoded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Uploading...`"
+        )
+
+    async def started_gen_ss(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Uploaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Generating Sample And Screen Shot...`"
+        )
+
+    async def all_done(self):
+        try:
+            self.msg = await self.msg.edit(
+                f"**Successfully Completed All Task Related To The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `DONE`"
+            )
+        except BaseException:
+            pass  # ValueError Sometimes From telethon
+        if Var.LOG_ON_MAIN:
+            await self.msg.delete()
+
+    async def awake(self):  # in case
+        if not self.client.is_connected():
+            await self.client.connect()
+
+    async def report_error(self, msg, log=False):
+        txt = f"[ERROR] {msg}"
+        if log:
+            LOGS.error(txt[0])
+        try:
+            await self.client.send_message(Var.LOG_CHANNEL, f"```{txt[:4096]}```")
+        except FloodWaitError as fwerr:
+            await self.client.disconnect()
+            LOGS.info("Sleeping Becoz Of Floodwait...")
+            await asyncio.sleep(fwerr.seconds + 10)
+            await self.client.connect()
+        except ConnectionError:
+            await self.client.connect()
+        except Exception as err:
+            LOGS.exception(format_exc())
+            LOGS.error(str(err))
---
+++ libs/subsplease.py
@@ -0,0 +1,76 @@
+
+
+import asyncio
+import hashlib
+import shutil
+import sys
+from itertools import count
+from traceback import format_exc
+
+import anitopy
+from feedparser import parse
+
+from database import LOGS, DataBase
+
+
+class SubsPlease:
+    def __init__(self, dB: DataBase):
+        self.db = dB
+
+    def digest(self, string: str):
+        return hashlib.sha256(string.encode()).hexdigest()
+
+    def _exit(self):
+        LOGS.info("Stopping The Bot...")
+        try:
+            [shutil.rmtree(fold) for fold in ["downloads", "thumbs", "encode"]]
+        except BaseException:
+            LOGS.error(format_exc())
+        sys.exit(0)
+
+    def rss_feed_data(self):
+        try:
+            return (
+                parse("https://subsplease.org/rss/?r=1080"),
+                parse("https://subsplease.org/rss/?r=720"),
+                parse("https://subsplease.org/rss/?r=sd"),
+            )
+        except KeyboardInterrupt:
+            self._exit()
+        except BaseException:
+            LOGS.error(format_exc())
+            return None, None, None
+
+    async def feed_optimizer(self):
+        d1080, d720, d480 = self.rss_feed_data()
+        if not d1080 or not d720 or not d480:
+            return None
+        for i in range(2, -1, -1):
+            try:
+                f1080, f720, f480 = d1080.entries[i], d720.entries[i], d480.entries[i]
+                a1080, a720, a480 = (
+                    (anitopy.parse(f1080.title)).get("anime_title"),
+                    (anitopy.parse(f720.title)).get("anime_title"),
+                    (anitopy.parse(f480.title)).get("anime_title"),
+                )
+                if a1080 == a720 == a480:
+                    if (
+                        "[Batch]" in f1080.title
+                        or "[Batch]" in f720.title
+                        or "[Batch]" in f480.title
+                    ):
+                        continue
+                    uid = self.digest(f1080.title + f720.title + f480.title)
+                    if not await self.db.is_anime_uploaded(uid):
+                        return {"uid": uid, "1080p": f1080, "720p": f720, "480p": f480}
+            except BaseException:
+                LOGS.error(format_exc())
+                return None
+
+    async def on_new_anime(self, function):
+        for i in count():
+            data = await self.feed_optimizer()
+            if data:
+                await function(data)
+                await self.db.add_anime(data.get("uid"))
+            await asyncio.sleep(5)
